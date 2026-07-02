591a670fc41b76ad606a41fbd93572a87b8fb0d6
aljaunia <abdullah.jauni@infratechton.com>
2026-07-02 19:59:40 +0200

    feat(09-02): export tool_call symbols from multimodal package
    - Added ToolCallDetection, ToolCallExtractor, ToolCallResult
    - Added extract_tool_calls_openai, extract_tool_calls_anthropic, extract_tool_calls_mcp

---
+++ src/anonreq/multimodal/__init__.py
@@ -0,0 +1,31 @@
+from anonreq.multimodal.dispatcher import ContentTypeDispatcher
+from anonreq.multimodal.json_analyzer import JsonAnalyzer
+from anonreq.multimodal.limits import LimitCheckResult, PayloadLimits, validate_payload_limits
+from anonreq.multimodal.models import AnalyzerResult, ContentType, UnifiedDetectionResult
+from anonreq.multimodal.multipart_analyzer import MultipartAnalyzer
+from anonreq.multimodal.tool_call import (
+    ToolCallDetection,
+    ToolCallExtractor,
+    ToolCallResult,
+    extract_tool_calls_anthropic,
+    extract_tool_calls_mcp,
+    extract_tool_calls_openai,
+)
+
+__all__ = [
+    "AnalyzerResult",
+    "ContentType",
+    "ContentTypeDispatcher",
+    "JsonAnalyzer",
+    "LimitCheckResult",
+    "MultipartAnalyzer",
+    "PayloadLimits",
+    "ToolCallDetection",
+    "ToolCallExtractor",
+    "ToolCallResult",
+    "UnifiedDetectionResult",
+    "extract_tool_calls_anthropic",
+    "extract_tool_calls_mcp",
+    "extract_tool_calls_openai",
+    "validate_payload_limits",
+]
